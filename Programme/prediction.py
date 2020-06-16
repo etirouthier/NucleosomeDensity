3ed4bb9a86c3076f9eb3bd2cc13e13b728413c9f
erouthier <etienne.routhier@upmc.fr>
2020-06-16 18:10:45 +0200

    Simplifying prediction

--- Programme/prediction.py
+++ Programme/prediction.py
@@ -12,7 +12,6 @@ import h5py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-im
 
 
 from keras.models import load_model
@@ -41,13 +40,6 @@ def parse_arguments(args=None):
                         (in seq_chr_sacCer3)""",
     )
     parser.add_argument(
-        "-f",
-        "--file",
-        nargs="+",
-        help="""CSV file containing the nucleosome occupancy
-                        on the whole genome.""",
-    )
-    parser.add_argument(
         "-s", "--seq2seq", action="store_true", help="If the model is a seq2seq model"
     )
     parser.add_argument(
@@ -57,74 +49,14 @@ def parse_arguments(args=None):
                         (only is seq2seq model)""",
     )
     parser.add_argument(
-        "-t",
-        "--training_set",
-        nargs="+",
-        default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-        help="""list of chromosome in the training set""",
-    )
-    parser.add_argument(
-        "-v",
-        "--validation_set",
-        nargs="+",
-        default=[14, 15],
-        help="""list of chromosome in the validation set""",
-    )
-    parser.add_argument(
         "--test",
         default="16",
         help="""chromosome on which to make prediction
                         (defaut 16 for S.cerevisiae)""",
     )
-    parser.add_argument(
-        "-n",
-        "--norm_max",
-        action="store_true",
-        help="""Normalizing the data by dividing by a rolling
-                        max""",
-    )
     parser.add_argument('-r', '--reversed_seq', action='store_true',
                         help='In order to predict the backward strand')
     return parser.parse_args(args)
-
-
-def _load_true(
-    proba_file,
-    half_wx,
-    output_len=1,
-    max_norm=False,
-    seq2seq=False,
-    windows_num=1,
-    training_set=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-    validation_set=[14, 15],
-    chromosome="16",
-):
-    proba = pd.read_csv(proba_file)
-    y_true = proba[proba.chr == "chr" + chromosome].value.values
-    threshold = nuc_occupancy(
-        proba_file, training_set, validation_set, return_threshold=True
-    )
-
-    if seq2seq:
-        if output_len % 2 == 0:
-            half_len = output_len // 2
-        else:
-            half_len = output_len // 2 + 1
-
-        y_true = y_true[
-            half_wx - half_len : windows_num * output_len + half_wx - half_len
-        ]
-
-    else:
-        y_true = y_true[half_wx:-half_wx]
-
-    y_true /= float(threshold)
-    y_true[y_true > 1] = 1
-
-    if max_norm:
-        y_true = _max_norm(y_true)
-    return y_true.reshape((len(y_true), 1))
-
 
 def load_data(seq2seq=False, args=None):
     window = 2001
@@ -176,33 +108,7 @@ def load_data(seq2seq=False, args=None):
         output_len = 1
         windows_num = 1
 
-    #y_true = _load_true(
-    #    proba_files[0],
-    #    half_wx,
-    #    args.norm_max,
-    #    output_len,
-    #    seq2seq,
-    #    windows_num,
-    #    args.training_set,
-    #    args.validation_set,
-    #    args.test,
-    #)
-    #for proba_file in proba_files[1:]:
-    #    y_true_ = _load_true(
-    #        proba_file,
-    #        half_wx,
-   #         args.norm_max,
-    #        output_len,
-   #         seq2seq,
-   #         windows_num,
-     #       args.training_set,
-     #       args.validation_set,
-       #     args.test,
-      #  )
-      #  y_true = np.append(y_true, y_true_, axis=1)
-
-    return X_#, y_true
-
+    return X_
 
 def main(command_line_arguments=None):
     arguments = parse_arguments(args=command_line_arguments)
@@ -240,20 +146,6 @@ def main(command_line_arguments=None):
     X_test = load_data(arguments.seq2seq, command_line_arguments)
     y_pred = model.predict(X_test)
     np.save(path_to_results, y_pred)
-
-    # fig = plt.figure(figsize=(10,10))
-
-    # for i in range(len(arguments.file)):
-    #    y_true[:, i][np.isnan(y_true[:, i])] = 0
-    #    correlation = pearsonr(y_pred[y_true[:, i] > 0, i], y_true[y_true[:, i] > 0, i])[0]
-    #    print('Correlation between true and pred n°{}:'.format(i), correlation)
-    #    ax = fig.add_subplot(len(arguments.file), 1, i + 1)
-    #    ax.plot(y_pred[:, i], 'b', label='prediction')
-    #    ax.plot(y_true[:, i], 'r', label='experimental')
-    #    ax.legend()
-    #    ax.set_title('Experimental and predicted occupancy' + \
-    #             'on chr 16 for model {}'.format(arguments.weight_file[6:]))
-    # plt.show()
 
 
 if __name__ == "__main__":
